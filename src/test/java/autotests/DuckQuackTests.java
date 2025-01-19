c73fb31ab943e3a98926b1f06d0a5624c120dd3f
vanon <vano.nikitin@gmail.com>
2025-01-20 03:35:16 +0700

    basic tests

---
+++ src/test/java/autotests/DuckQuackTests.java
@@ -0,0 +1,102 @@
+package autotests;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.testng.annotations.Optional;
+
+import org.testng.annotations.Test;
+
+import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+public class DuckQuackTests extends TestNGCitrusSpringSupport{
+    @Test(description = "Корректный нечётный id, корректный звук", priority = 1)
+    @CitrusTest
+    public void quackOdd(@Optional @CitrusResource TestCaseRunner runner) {
+        createDuck(runner, "yellow", 0.15, "rubber", "quack", "FIXED");
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .extract(fromBody().expression("$.id", "duckId")));
+
+        runner.$(action -> {
+            if(Integer.parseInt(action.getVariable("duckId")) % 2 == 0){
+                createDuck(runner, "yellow", 0.15, "rubber", "quack", "FIXED");
+
+                http().client("http://localhost:2222")
+                        .receive()
+                        .response(HttpStatus.OK)
+                        .message()
+                        .extract(fromBody().expression("$.id", "duckId"));
+            }
+        });
+
+        duckQuack(runner, "${duckId}", "2", "3");
+        validateResponse(runner, "{\n" + "  \"sound\": \"quack-quack-quack, quack-quack-quack\"," + "}"
+        );
+    }
+
+    @Test(description = "Корректный чётный id , корректный звук", priority = 2)
+    @CitrusTest
+    public void quackEven(@Optional @CitrusResource TestCaseRunner runner) {
+        createDuck(runner, "yellow", 0.15, "rubber", "quack", "FIXED");
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .extract(fromBody().expression("$.id", "duckId")));
+
+        runner.$(action -> {
+            if(Integer.parseInt(action.getVariable("duckId")) % 2 != 0){
+                createDuck(runner, "yellow", 0.15, "rubber", "quack", "FIXED");
+
+                http().client("http://localhost:2222")
+                        .receive()
+                        .response(HttpStatus.OK)
+                        .message()
+                        .extract(fromBody().expression("$.id", "duckId"));
+            }
+        });
+        duckQuack(runner, "${duckId}", "2", "3");
+        validateResponse(runner, "{\n" + "  \"sound\": \"quack-quack-quack, quack-quack-quack\"," + "}"
+        );
+    }
+
+    public void duckQuack(TestCaseRunner runner, String id, String repetitionCount, String soundCount) {
+        runner.$(http().client("http://localhost:2222")
+                .send()
+                .get("/api/duck/action/quack")
+                .queryParam("id", id)
+                .queryParam("repetitionCount", repetitionCount)
+                .queryParam("soundCount", soundCount));
+    }
+
+    public void validateResponse(TestCaseRunner runner, String responseMessage) {
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE).body(responseMessage));
+    }
+
+
+    public void createDuck(TestCaseRunner runner, String color, double height, String material, String sound, String wingsState) {
+        runner.$(http().client("http://localhost:2222")
+                .send()
+                .post("/api/duck/create")
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("{\n" + "  \"color\": \"" + color + "\",\n"
+                        + "  \"height\": " + height + ",\n"
+                        + "  \"material\": \"" + material + "\",\n"
+                        + "  \"sound\": \"" + sound + "\",\n"
+                        + "  \"wingsState\": \"" + wingsState
+                        + "\"\n" + "}"));
+    }
+}
+
