ba54ba34532802d0e762f35e853e5b6083f45362
vanon <vano.nikitin@gmail.com>
2025-01-24 00:25:38 +0700

    clients + EndpointConfig + DuckActionsClient

---
+++ src/test/java/autotests/tests/DuckCreateTests.java
@@ -0,0 +1,26 @@
+package autotests.tests;
+
+import autotests.clients.DuckActionsClient;
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+
+
+public class DuckCreateTests extends DuckActionsClient {
+    @Test(description = "Утка с material = rubber")
+    @CitrusTest
+    public void properRubber(@Optional @CitrusResource TestCaseRunner runner) {
+        createDuck(runner, "yellow", 0.15, "rubber", "quack", "FIXED");
+        validateResponse(runner, "create", HttpStatus.OK, "yellow",0.15, "rubber", "quack", "FIXED");
+    }
+
+    @Test(description = "Утка с material = wood")
+    @CitrusTest
+    public void properWood(@Optional @CitrusResource TestCaseRunner runner) {
+        createDuck(runner, "yellow", 0.15, "wood", "quack", "FIXED");
+        validateResponse(runner, "create", HttpStatus.OK,"yellow", 0.15, "wood", "quack", "FIXED");
+    }
+}
---
+++ src/test/java/autotests/tests/DuckSwimTests.java
@@ -0,0 +1,31 @@
+package autotests.tests;
+
+import autotests.clients.DuckActionsClient;
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+
+
+public class DuckSwimTests extends DuckActionsClient {
+
+    @Test(description = "Существующий id")
+    @CitrusTest
+    public void swimExist(@Optional @CitrusResource TestCaseRunner runner) {
+        createDuck(runner, "yellow", 0.15, "wood", "quack", "FIXED");
+        getId(runner);
+        duckSwim(runner, "${duckId}");
+        validateResponse(runner,  "{\n" + "  \"message\": \"I’m swimming\"\n" + "}", HttpStatus.OK, "",null,"","","");
+    }
+
+    @Test(description = "Несуществующий id")
+    @CitrusTest
+    public void swimNotExist(@Optional @CitrusResource TestCaseRunner runner) {
+        createDuck(runner, "yellow", 0.15, "wood", "quack", "FIXED");
+        getId(runner);
+        duckSwim(runner, "-1");
+        validateResponse(runner,  "{\n" + "  \"message\": \"Paws are not found ((((\"\n" + "}", HttpStatus.valueOf(400), "",null,"","","");
+    }
+}
